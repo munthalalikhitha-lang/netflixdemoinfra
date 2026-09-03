c280b5a77af094704f58adb46ac3634442d84334
munthalalikhitha-lang <munthalalikhitha@gmail.com>
2026-09-03 23:02:36 +0530

    Update main.tf

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "munthalalikhitha-terraform-state-2026"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
